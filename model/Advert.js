8778d1544bcb1649a8b0707dcc5d0b8b2afc1a7c
aliAkkayaJ <aliakkayaa0@gmail.com>
2024-03-10 10:29:31 +0300

    ilan şemasına title eklendi

--- model/Advert.js
+++ model/Advert.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const advertSchema = new mongoose.Schema(
   {
+    title: {
+      type: String,
+      required: true,
+    },
     description: {
       type: String,
       required: true,
