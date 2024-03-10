15c97b3ab6405b0e0c41c830e5e0cc03d67e728e
aliAkkayaJ <aliakkayaa0@gmail.com>
2024-03-10 05:58:54 +0300

    mesajları database'de tutuyoruz

--- server.js
+++ server.js
@@ -16,34 +16,41 @@ const io = socketIo(server, {
   },
 });
 
-const messageHistory = {}; // Odalara göre mesajları saklamak için bir nesne
-
 io.on("connection", (socket) => {
   socket.on("join", (room) => {
     socket.join(room);
-    // Kullanıcı odalara katıldığında, geçmiş mesajları kontrol ederek onlara gönder
-    if (messageHistory[room]) {
-      messageHistory[room].forEach((message) => {
-        socket.emit("chat message", message);
-      });
-    }
+    Message.find({ room }, (err, messages) => {
+      if (err) {
+        console.error("Mesajlar alınırken bir hata oluştu:", err);
+      } else {
+        messages.forEach((message) => {
+          socket.emit("chat message", message.message);
+        });
+      }
+    });
   });
 
   console.log("New client connected", socket.id);
 
-  socket.on("chat message", (msg, room) => {
+  socket.on("chat message", (msg, room, sender) => {
     if (room && io.sockets.adapter.rooms.get(room)) {
-      // Eğer belirtilen bir oda varsa, sadece o odaya mesaj gönder
+      const newMessage = new Message({ room, sender, message: msg });
+      newMessage.save((err) => {
+        if (err) {
+          console.error("Mesaj kaydedilirken bir hata oluştu:", err);
+        }
+      });
       io.to(room).emit("chat message", msg);
     } else {
-      // Eğer belirtilen oda yoksa, yeni bir oda oluştur ve mesajı odaya gönder
       const newRoom =
         room || "generatedRoom_" + Math.random().toString(36).substring(7);
       socket.join(newRoom);
-      if (!messageHistory[newRoom]) {
-        messageHistory[newRoom] = []; // Yeni oda için mesaj geçmişini oluştur
-      }
-      messageHistory[newRoom].push(msg); // Mesajı geçmişe ekle
+      const newMessage = new Message({ room: newRoom, sender, message: msg });
+      newMessage.save((err) => {
+        if (err) {
+          console.error("Mesaj kaydedilirken bir hata oluştu:", err);
+        }
+      });
       io.to(newRoom).emit("chat message", msg);
     }
   });
@@ -52,7 +59,6 @@ io.on("connection", (socket) => {
     console.log("Client disconnected");
   });
 });
-
 server.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
