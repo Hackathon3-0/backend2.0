3e8059717e2244c6059b3c55070b890e8e646923
aliAkkayaJ <aliakkayaa0@gmail.com>
2024-03-10 05:02:04 +0300

    socketio-real time chat eklendi

--- server.js
+++ server.js
@@ -1,11 +1,57 @@
 require("dotenv").config();
-const http = require("http");
-const app = require("./app/app");
 require("./config/dbConnect");
 
-const PORT = process.env.PORT || 2020;
+const http = require("http");
+const socketIo = require("socket.io");
 
+const app = require("./app/app");
+
+const PORT = process.env.PORT || 2020;
 const server = http.createServer(app);
+
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const messageHistory = {}; // Odalara göre mesajları saklamak için bir nesne
+
+io.on("connection", (socket) => {
+  socket.on("join", (room) => {
+    socket.join(room);
+    // Kullanıcı odalara katıldığında, geçmiş mesajları kontrol ederek onlara gönder
+    if (messageHistory[room]) {
+      messageHistory[room].forEach((message) => {
+        socket.emit("chat message", message);
+      });
+    }
+  });
+
+  console.log("New client connected", socket.id);
+
+  socket.on("chat message", (msg, room) => {
+    if (room && io.sockets.adapter.rooms.get(room)) {
+      // Eğer belirtilen bir oda varsa, sadece o odaya mesaj gönder
+      io.to(room).emit("chat message", msg);
+    } else {
+      // Eğer belirtilen oda yoksa, yeni bir oda oluştur ve mesajı odaya gönder
+      const newRoom =
+        room || "generatedRoom_" + Math.random().toString(36).substring(7);
+      socket.join(newRoom);
+      if (!messageHistory[newRoom]) {
+        messageHistory[newRoom] = []; // Yeni oda için mesaj geçmişini oluştur
+      }
+      messageHistory[newRoom].push(msg); // Mesajı geçmişe ekle
+      io.to(newRoom).emit("chat message", msg);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
 server.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
